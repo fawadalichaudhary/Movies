5634e34c5b41dd2c7bf4ed136342c939258501ee
Fawad ALi <fawadalichaudhary97@gmail.com>
2026-04-07 18:32:45 +0500

    search query

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -6,24 +6,37 @@ const Movie = () => {
   const [data, setdata] = useState([])
   const [search, setSearch] = useState("");
 
-  const movies = "https://api.themoviedb.org/3/discover/movie?api_key=248bef0b9b0a772b3d5bc1933b433de1"
+  const discoverURL = "https://api.themoviedb.org/3/discover/movie?api_key=248bef0b9b0a772b3d5bc1933b433de1"
+  const searchURL = `https://api.themoviedb.org/3/search/movie?api_key=248bef0b9b0a772b3d5bc1933b433de1&query=${search}`
+
 
   useEffect(() => {
     const getdata = async () => {
       try {
-        const res = await axios.get(movies)
+        const res = await axios.get(discoverURL)
         setdata(res.data.results)
+        console.log(res.data.results);
 
       } catch (error) {
         console.log(error);
-
       }
-
     }
     getdata()
   }, [])
 
-  const filltered = data.filter((fillter) => fillter.title.toLowerCase().includes(search.toLowerCase()))
+  const searchfunct = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await axios.get(searchURL)
+      setdata(res.data.results)
+
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  }
 
 
   return (
@@ -31,12 +44,13 @@ const Movie = () => {
       <div className="bg-blue-900 min-h-screen">
         <div className="flex justify-between bg-blue-950 p-1.5">
           <h1 className="text-white">Movie Search</h1>
-
-          <input type="text" placeholder="Search movie..." className="w-full max-w-80" value={search}
-            onChange={(e) => setSearch(e.target.value)} />
+          <form onSubmit={searchfunct}>
+            <input type="text" placeholder="Search movie..." className="w-full max-w-80 text-white" value={search}
+              onChange={(e) => setSearch(e.target.value)} />
+          </form>
         </div>
-        <div className="flex gap-3.5 flex-wrap p-5 items-center justify-center">
-          {filltered.map((movie) => (
+        <div className="flex gap-3.5 flex-wrap p-5 justify-center">
+          {data.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
         </div>
--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,17 +1,19 @@
+import FallbackImage from "../assets/fallback_image.webp"
+
 const MovieCard = ({ movie }) => {
     return (
-        <div className="bg-blue-950 cursor-pointer w-43">
+        <div className="relative bg-blue-950 cursor-pointer w-43">
             <img
-                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                src={movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : FallbackImage}
                 alt={movie.title}
-                className="w-full h-auto object-fill"
+                className="w-full aspect-2/3 object-cover"
             />
             <div className="p-4 flex justify-between items-start gap-2">
                 <h3 className="text-white font-bold text-sm leading-tight">{movie.title}</h3>
-                <span className={`px-2 py-1 rounded text-xs font-bold ${movie.vote_average >= 7 ? 'bg-blue-600 text-white' : 'bg-red-500 text-white'}`}>
-                    {movie.vote_average}
-                </span>
             </div>
+            <span className={`absolute top-2 right-2 px-2 py-1 rounded text-xs font-bold ${movie.vote_average >= 7 ? 'bg-blue-600 text-white' : 'bg-red-500 text-white'}`}>
+                {movie.vote_average}
+            </span>
         </div>
     );
 };
